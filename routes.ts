514ebad9b83498daebd5285751efab6a98f6acfc
rayrahadii <rahadisasmita@gmail.com>
2020-06-25 00:05:47 +0700

    Routes File To String

--- routes.ts
+++ routes.ts
@@ -1,5 +1,5 @@
 import {RouterContext} from "https://deno.land/x/oak@v5.3.1/mod.ts";
-import {renderFileToString} from "https://deno.land/x/dejs@0.7.0/mod.ts"
+import {renderFileToString} from "https://deno.land/x/dejs@0.7.0/mod.ts";
 
 export const home = async (ctx: RouterContext) => {
     ctx.response.body = await renderFileToString(`${Deno.cwd()}/views/home.ejs`,
